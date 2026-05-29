17b2199528d9ff8f796fef18c97a5ba09480006d
GUSTAVO RODRIGUES PADOVAN PEREZ <54869342898@info.local>
2026-05-29 09:09:43 -0300

    codigos e comandos da TV adicionados

--- src/main.cpp
+++ src/main.cpp
@@ -22,20 +22,52 @@ const char TOPICO_COMANDO[] = "senai134/mbolas/televisao";
 void tratarJsonComando(const String& mensagem);
 void conectarTelevisao();
 
+struct ComandoIR {
+  const char* nome;
+  uint32_t    codigo;
+};
+ 
+const ComandoIR tabelaComandos[] = {
+  { "power",           0x20DF10EF },
+  { "baixar_volume",   0x20DF40BF },
+  { "aumentar_volume", 0x20DFC03F },
+  {"botao_ok",         0x20DF22DD },
+  { "mute",            0x20DF906F },
+  { "hdmi1",           0x20DF738C },
+  { "hdmi2",           0x20DFB34C },
+  { "botão_cima",       0x20DF02FD },
+  { "botão_baixo",      0x20DF827D },
+  { "botão_esquerda",   0x20DFE01F },
+  { "botão_direita",    0x20DF609F },
+  { "1",                0x20DF8877 },
+  { "2",                0x20DF48B7 },
+  { "3",                0x20DFC837 },
+  { "4",                0x20DF28D7 },
+  { "5",                0x20DFA857 },
+  { "6",                0x20DF6897 },
+  { "7",                0x20DFE817 },
+  { "8",                0x20DF18E7 },
+  { "9",                0x20DF9867 },
+  { "0",                0x20DF08F7 },
+  };
+  
+
 void setup() {
 
   Serial.begin(9600);
+  irsend.begin();         
+  irrecv.enableIRIn();
   conectarWiFi();
   configurarMQTT();
   conectarMQTT();
-  irrecv.enableIRIn();
-  Serial.println("Pronto! Pressione o botão BOOT para ligar/desligar a TV.");
+  debugInfo("Pronto! Aguardando comandos MQTT...");
 }
 
 void loop() {
-   garantirWifiConectado();
-   void garantirMQTTConectado();
-   loopMQTT();
+  garantirWifiConectado();
+  garantirMQTTConectado();
+  loopMQTT();
+  receberSinalInfraRed();
 }
 
 void tratarMensagemRecebida(const char *topico, const String &mensagem)
@@ -61,22 +93,21 @@ void tratarMensagemRecebida(const char *topico, const String &mensagem)
   debugErro("Topico não tratado: " + String(topico));
 } 
 
-void conectarTelevisao()
+
+void conectarTelevisao(uint32_t codigo)
 {
-  //!PRECISA DA CONFIRMAÇÃO PARA SER ENVIADO (COM UM IF)
-  Serial.println("Botão pressionado! Enviando sinal IR...");
+  debugInfo("Enviando sinal IR...");
 
-    irrecv.pause();                      //Pausa o receptor para não capturar o próprio sinal
-    irsend.sendNEC(0x20DF40BF, 32);   //! SUBSTITUIR PELO JSON  
-    delay(100);
-    irrecv.resume();                     //Reativa o receptor após o envio
+  irrecv.pause();               // Pausa o receptor para não capturar o próprio sinal
+  irsend.sendNEC(codigo, 32);   // Envia o código recebido via JSON
+  delay(100);
+  irrecv.resume();              // Reativa o receptor após o envio
 
-    Serial.println("Sinal enviado!");
-    delay(200);  // debounce do botão
-  
+  debugInfo("Sinal IR enviado: 0x" + String(codigo, HEX));
 }
 
-void receberSinalInfraRed() //* Se apontar o controle e dar o sinal, ira apareecer no terminal
+
+void receberSinalInfraRed() //* Se apontar o controle e dar o sinal, ira aparecer dentro do terminal
 {
     if (irrecv.decode(&results)) {
     Serial.print("Sinal recebido: 0x");
